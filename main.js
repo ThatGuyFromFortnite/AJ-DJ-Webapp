9a04fdd048f10390838e38e279e5679c6d2a49ba
VenkateshWhiteHatJR <78906730+VenkateshWhiteHatJR@users.noreply.github.com>
2021-02-14 19:08:26 +1000

    Update main.js

--- main.js
+++ main.js
@@ -104,3 +104,34 @@ function play()
 	song.setVolume(1);
 	song.rate(1);
 }
+
+function setup () {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+    
+    video = createCapture(VIDEO);
+    video.hide();
+    
+    poseNet = ml5.posenet(video , modeloaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('Posenet Is Initiated')
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+        {
+            console.log(results);
+            leftWristX = results[0].pose.leftWrist.x;
+            leftWristY = results[0].pose.leftWrist.y;
+            console.log("leftWristX =" + leftWristX +" leftWristY = "+ leftWristY);
+            
+            rightWristX = results[0].pose.rightWrist.x;
+            rightWristY = results[0].pose.rightWrist.y;
+            console.log("leftWristX =" + rightWristX +" rightWristY = "+ rightWristY);
+            
+        }
+}
